d760173178fdcc0089a9e897474ea37e5e0b59fe
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2020-03-17 16:17:02 +0100

    Relation between models

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+    /**
+     * @return Boolean
+     */
+    public function isAvailable()
+    {
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+    public function seller()
+    {
+        $this->belongsTo(Seller::class);
+    }
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
