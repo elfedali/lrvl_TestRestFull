af3df0abc9e83b9f1851d56bd6a89b3bd43fbb52
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2020-03-22 14:29:07 +0100

    Implementing Complex Operations for Buyer

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,8 +2,9 @@
 
 namespace App\Traits;
 
-use Illuminate\Database\Eloquent\Collection;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 /**
  * Response en json 200/2001/404 ...
