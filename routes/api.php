a17dd302f5b5fa89d26d748ca5baf35fc0ca8e43
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2020-03-24 17:06:52 +0100

    Implementing Complex Operations for Seller

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,10 @@ Route::resource('products', 'Product\ProductController', ['only'=>['index','show
  * Sellers
  */
 Route::resource('sellers', 'Seller\SellerController', ['only'=>['index','show']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except'=>['create','show','edit']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only'=>['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only'=>['index']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only'=>['index']]);
 /**
  * Transactions
  */
