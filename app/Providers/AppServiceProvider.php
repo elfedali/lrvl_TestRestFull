53cd6de892cb78c3e57eaa05ee4cda21efc2a051
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2020-03-25 12:37:06 +0100

    Handling the Products Availability Using Events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-
+use App\Product;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +16,16 @@ class AppServiceProvider extends ServiceProvider
     {
 
         //Schema::defaultStringLength(191);
+
+        /**
+         * Trigger this event when $product is updated 
+         */
+        Product::updated(function ($product) {
+            if ($product->quantity == 0 && $product->status == Product::AVAILABLE_PRODUCT) :
+                $product->status = Product::UNAVAILABLE_PRODUCT;
+                $product->save();
+            endif;
+        });
     }
 
     /**
